b43b7d9530debed00e7f45f5b9154d081d6fe1d8
Hassan <pkhajeh@gmail.com>
2019-10-21 10:50:04 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I want to try Git");
+        System.out.println("I think I can manage it");
     }
 }
